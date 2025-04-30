62e2c4bffa7279ffd18ecaa635911499395f2d17
alberto mata <al911mata@gmail.com>
2025-04-30 12:14:16 -0700

    Adding logger details

--- ILogger/ILogger_test.cs
+++ ILogger/ILogger_test.cs
@@ -7,10 +7,11 @@ namespace LoggingTest
     //Define an enum for log levels
     public enum LogLevel
     {
+        //list all the types of errors that can be captured. 
         Debug,
-        Information,
+        Information,        
         Warning,
-        error,
+        Error,
         Critical        
     }
 
@@ -20,32 +21,50 @@ namespace LoggingTest
     {
         public void Log(LogLevel level, string message);
         public void Log(LogLevel level, string message, Exception exception);
-        // public void Log(string msg);
-        // public void Log(string msgType, string msg);
-        // public void InitLogSession();
-        // public void EndLogSession();
-        // public void AddLogger(Logger appendedLogger);
-        // public void RemoveLogger(Logger appendedLogger);
     }
     
     //Sample class using the ILogger interface
     public class FileLogger : Logger
     {
+        //implement methods
         public void Log(LogLevel level, string message)
-        //implement methods
+        {
+            Console.WriteLine($"[{DateTime.Now}] [{level}]: {message}")
+        }
+
+        public void Log(LogLevel level, string message, Exception exception)
+        {
+            Conosole.WriteLine($"[{DateTime.Now}] [{level}]: {message}")
+            Console.WriteLine($"  Exception: {exception}");
+        }
     }
 
-    public class TextBoxLogger : Logger
+    public class TextBoxLogger
     {
         //implement methods
+        private readonly ILogger _logger;
+        
+        public TextBoxLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void MessageFromLogger()
+        {
+            _logger.Log(LogLevel.Information, "We are sending log message about log captured as INFO");
+            _logger.Log(LogLevel.Warning, "Here is a warning log event captured in application");
+            
+            //write try catch for critical errors logged/captured
+            try
+            {
+                throw new Exception("An error happened");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, "A bad error has occurred", ex);
+            }
+        }
     }
-
-    public class DBLogger : Logger
-    {
-        //implement methods
-    }
-
-
 }
 
 
